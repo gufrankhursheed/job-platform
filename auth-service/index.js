65b3ed61765afaf6bb3ada3e68986061cc0d3111
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-04-23 14:41:22 +0530

    auth-server connected to DB

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,10 +1,17 @@
-import dotenv from "dotenv"
-import app from "./app.js"
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDb from "./src/db/index.js";
 
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
-app.listen(process.env.PORT || 5001, () => {
-    console.log(`Auth-server is running on port ${process.env.PORT}`)
+connectDb()
+.then(() => {
+  app.listen(process.env.PORT || 5001, () => {
+    console.log(`Auth-server is running on port ${process.env.PORT}`);
+  });
+})
+.catch((error) => {
+    console.log("Error: ", error)
 })
