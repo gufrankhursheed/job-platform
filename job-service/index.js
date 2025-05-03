8a1c26465f88c19b741fd2e06761f854285f66a6
unknown <gufrankhursheed7@gmail.com>
2025-05-03 16:05:22 +0530

    job-service add DB connection

--- job-service/index.js
+++ job-service/index.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv"
 import app from "./app.js"
+import { connectDb } from "./src/db/index.js"
 
 dotenv.config({
     path: "./.env"
 })
 
-app.listen(process.env.PORT || 5003, () => {
-    console.log(`Job server is running on port ${process.env.PORT}`)
+connectDb()
+.then(() => {
+    app.listen(process.env.PORT || 5003, () => {
+        console.log(`Job server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("Error: ", error)
 })
