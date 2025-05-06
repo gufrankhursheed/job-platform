333ae8ae11b6414376a7489ed65ed6094de2415e
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-05-06 18:12:00 +0530

    application-server add query applications

--- application-service/src/controllers/application.controller.js
+++ application-service/src/controllers/application.controller.js
@@ -27,13 +27,63 @@ const applyJob = async(req, res) => {
 
         return res.status(200).json({message: "Application submitted", application})
     } catch (error) {
-        console.log("Job creation failed:", error)
+        console.log("Application submission failed:", error)
         return res.status(400).json({ error: error })
     }
 }
 
+const getCandidateApplication = async(req, res) => {
+    try {
+        const { candidateId } = req.params
 
+        if(!candidateId) {
+            return res.status(400).json({message: "Candidate ID is missing"})
+        }
+
+        const applications = await Application.findAll({
+            where: {
+                candidateId: candidateId
+            }
+        })
+
+        if(!applications) {
+            return res.status(400).json({message: "No applications found"})
+        }
+
+        return res.status(200).json({message: "Applications retrieved successfully", applications})
+    } catch (error) {
+        console.error("Applications retrieve failed", error)
+        return res.status(400).json({ error: error })
+    }
+}
+
+const getJobApplication = async(req, res) => {
+    try {
+        const { jobId } = req.params
+
+        if(!jobId) {
+            return res.status(400).json({message: "Job ID is missing"})
+        }
+
+        const applications = await Application.findAll({
+            where: {
+                jobId: jobId
+            }
+        })
+
+        if(!applications) {
+            return res.status(400).json({message: "No applications found for this job"})
+        }
+
+        return res.status(200).json({message: "Applications retrieved successfully", applications})
+    } catch (error) {
+        console.error("Applications retrieve failed", error)
+        return res.status(400).json({ error: error })
+    }
+}
 
 export {
-    applyJob
+    applyJob,
+    getCandidateApplication,
+    getJobApplication
 }
--- application-service/src/routes/application.route.js
+++ application-service/src/routes/application.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { applyJob } from "../controllers/application.controller";
+import { applyJob, getCandidateApplication, getJobApplication } from "../controllers/application.controller";
 
 const router = Router()
 
 router.route("/").post(applyJob)
+router.route("/candidate/:candidateId").get(getCandidateApplication)
+router.route("/job/:jobId").get(getJobApplication)
 
 export default router
