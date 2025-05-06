11a0aa0e23da5edf48c40f994c917355f879dc33
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-05-06 15:23:01 +0530

    application-server add  application model

---
+++ application-service/src/models/application.model.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db";
+
+const Application = sequelize.define('Application', {
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'applied'
+    },
+    resumeScore: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    appliedDate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    candidateId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+      jobId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+})
+
+export default Application
