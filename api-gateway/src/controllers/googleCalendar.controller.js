44dea707019b4d798c22467fe104a17474d9f721
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-05-19 14:36:25 +0530

    add updateCalendarEvent

--- api-gateway/src/controllers/googleCalendar.controller.js
+++ api-gateway/src/controllers/googleCalendar.controller.js
@@ -50,6 +50,51 @@ const createCalendarEvent = async(req, res) => {
     }
 }
 
+const updateCalendarEvent = async(req, res) => {
+    try {
+        const { userId, updatedDetails, eventId } = req.body
+
+        if(!userId || !eventDetails){
+            return res.status(400).json({ message: "Missing userId or event details" })
+        }
+
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.status(400).json({ message: "User not found" })
+        }
+
+        if(!user.google?.accessToken || !user.google.refreshToken) {
+            return res.status(400).json({ message: "User not connected to google" })
+        }
+
+        if(!eventId){
+            return res.status(400).json({ message: "Calendar Event Id is missing" })
+        }
+
+        oAuth2Client.setCredentials({
+            access_token: user.google.accessToken,
+            refresh_token: user.google.refreshToken
+        })
+
+        const calendar = google.calendar({ version: "v3", auth: oAuth2Client })
+
+        const response = await calendar.events.update({
+            calendarId: "primary",
+            eventId: eventId,
+            requestBody: {
+                ...updatedDetails,
+            }
+        })
+
+        return res.status(200).json({ message: "Calendar event updated", data: response.data})
+    } catch (error) {
+        console.log("Calendar event update failed:", error)
+        return res.status(400).json({error: error})
+    }
+}
+
 export {
-    createCalendarEvent
+    createCalendarEvent,
+    updateCalendarEvent
 }
