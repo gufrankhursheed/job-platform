9356593a50dd5424a1721b5d088e002c441d0a5e
Md Gufran Khursheed <gufrankhursheed7@gmail.com>
2025-05-21 12:48:42 +0530

    chat-service connect to DB

--- chat-service/index.js
+++ chat-service/index.js
@@ -2,9 +2,16 @@ import app from "./app.js";
 import "./src/config/env.js";
 
 import http from "http"
+import connectDb from "./src/db/index.js";
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 5004, () => {
-    console.log(`Chat server is running on port ${process.env.PORT}`)
+connectDb()
+.then(() => {
+    server.listen(process.env.PORT || 5004, () => {
+        console.log(`Chat server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("Error: ", error)
 })
